b630680a629d9ddc239e5eff76541f12db46c7a6
Francois Botha <gitkraken.flint995@passmail.net>
2025-11-24 15:04:44 +0200

    Day 1 2015

---
+++ src/bin/01.rs
@@ -0,0 +1,55 @@
+use std::process::Output;
+
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut floor = 0;
+    for c in input.chars() {
+        if c == '(' {
+            floor += 1;
+        }
+        if c == ')' {
+            floor -= 1;
+        }
+    }
+
+    return Some(floor);
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut floor = 0;
+    let mut res = 0;
+    for (i, c) in input.chars().enumerate() {
+        if c == '(' {
+            floor += 1;
+        }
+        if c == ')' {
+            floor -= 1;
+        }
+
+        if(floor < 0)
+        {
+            res = i+1;
+            break;
+        }
+    }
+
+    return Some(res as u64);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(5));
+    }
+}
